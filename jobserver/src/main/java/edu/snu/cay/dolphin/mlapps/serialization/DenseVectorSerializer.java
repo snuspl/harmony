7aa4b5946d9186d394eaa4d91687fe3d8823b424
WooYeon <wylee.xyzi@gmail.com>
2018-03-23 12:29:17 +0900

    Use Kryo

---
+++ jobserver/src/main/java/edu/snu/cay/dolphin/mlapps/serialization/DenseVectorSerializer.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2018 Seoul National University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.snu.cay.dolphin.mlapps.serialization;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.Serializer;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
+import edu.snu.cay.common.math.linalg.Vector;
+import edu.snu.cay.common.math.linalg.VectorFactory;
+
+import javax.inject.Inject;
+import java.util.logging.Logger;
+
+/**
+ * Created by xyzi on 09/01/2018.
+ */
+public final class DenseVectorSerializer extends Serializer<Vector> {
+  private static final Logger LOG = Logger.getLogger(DenseVectorSerializer.class.getName());
+
+  private final VectorFactory vectorFactory;
+
+  @Inject
+  private DenseVectorSerializer(final VectorFactory vectorFactory) {
+    this.vectorFactory = vectorFactory;
+  }
+
+  @Override
+  public void write(final Kryo kryo, final Output output, final Vector vector) {
+    if (!vector.isDense()) {
+      LOG.warning("the given vector is not dense.");
+    }
+
+    output.writeInt(vector.length());
+    for (int i = 0; i < vector.length(); i++) {
+      output.writeFloat(vector.get(i));
+    }
+  }
+
+  @Override
+  public Vector read(final Kryo kryo, final Input input, final Class<Vector> type) {
+    final int vecSize = input.readInt();
+    final Vector vector = vectorFactory.createDenseZeros(vecSize);
+    for (int i = 0; i < vecSize; i++) {
+      vector.set(i, input.readFloat());
+    }
+    return vector;
+  }
+}
---
+++ jobserver/src/main/java/edu/snu/cay/dolphin/mlapps/serialization/Kryos.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2018 Seoul National University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.snu.cay.dolphin.mlapps.serialization;
+
+import com.esotericsoftware.kryo.Kryo;
+
+/**
+ * Created by xyzi on 23/03/2018.
+ */
+public final class Kryos {
+  // configure kryo instance, customize settings
+  // Setup ThreadLocal of Kryo instances
+  private static final ThreadLocal<Kryo> KRYOS = ThreadLocal.withInitial(Kryo::new);
+
+  // Utility classes should not have a public or default constructor.
+  private Kryos() {
+
+  }
+
+  /**
+   *
+   * @return
+   */
+  public static Kryo get() {
+    return KRYOS.get();
+  }
+}
